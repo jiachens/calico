8bdb0e3152e2f1633e25d64e9031a3f9c0adca83
Jiachen Sun <jcsun0806@gmail.com>
2022-12-15 04:09:19 +0000

    fix bug

--- mmdet3d/datasets/pipelines/transforms_3d.py
+++ mmdet3d/datasets/pipelines/transforms_3d.py
@@ -522,16 +522,16 @@ class BBoxTransformer:
         """
         Private function to generate random pooled bbox.
         """
-        #FIXME: this is a hack to make sure the bbox is not too close to the edge
         h = np.random.uniform(0.5, 5, num_bbox) # empirically chosen height
         w = np.random.uniform(0.5, 5, num_bbox)
         x1 = np.random.uniform(-bev_range[0], bev_range[2]-5, num_bbox)
         y1 = np.random.uniform(-bev_range[1], bev_range[3]-5, num_bbox)
         x2 = x1 + w
         y2 = y1 + h
-        bbox = np.concatenate([x1, y1, x2, y2], axis=1)
+        bbox = np.stack([x1, y1, x2, y2], axis=1)
         device = torch.device("cpu")
-        return torch.as_tensor(bbox,dtype=torch.float32, device=device)
+        bbox = torch.as_tensor(bbox,dtype=torch.float32, device=device)
+        return bbox.view(-1,4)
 
     def __call__(self,data):
         assert ("pooled_bbox" in data), "pooled_bbox should be in data"
