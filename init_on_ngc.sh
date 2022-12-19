603f496f703f9b91f573bd1185004b995770e37b
Jiachen Sun <jcsun0806@gmail.com>
2022-12-19 04:45:22 +0000

    fix bugs

--- init_on_ngc.sh
+++ init_on_ngc.sh
@@ -1,5 +1,7 @@
 git pull
 pip install setuptools==59.5.0
 ln -s /workspace/nuscenes ./data/nuscenes
+# For V100 CUDA11.0 
 pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip uninstall -y mmcv-full 
 pip install mmcv-full==1.4.0 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
