8a922adc0b15959d9e949a030ae35639ad45df22
Jiachen Sun <jcsun0806@gmail.com>
2022-12-20 01:28:14 +0000

    fix bugs in clip loss (norm)

--- mmdet3d/models/projectors/cnn.py
+++ mmdet3d/models/projectors/cnn.py
@@ -32,3 +32,32 @@ class CNNProjector(nn.Module):
         x = F.adaptive_max_pool2d(x, 1).squeeze()
         x = self.last(x)
         return x
+
+
+# @PROJECTORS.register_module()
+class SharedProjector(nn.Module):
+    def __init__(self,
+                 channels,
+                 kernel_size,
+                 stride,
+                 ):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for i in range(len(channels) - 1):
+            self.layers.append(
+                nn.Sequential(
+                    nn.Conv2d(channels[i], channels[i + 1], kernel_size=kernel_size, stride=stride, padding='valid'),
+                    nn.BatchNorm2d(channels[i + 1]),        
+                    nn.ReLU()        
+                    )
+            )
+        self.last_lidar = nn.Linear(channels[-1], channels[-1])
+        self.last_camera = nn.Linear(channels[-1], channels[-1])
+
+    @force_fp32(apply_to=('x',))
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        x = F.adaptive_max_pool2d(x, 1).squeeze()
+        x = self.last(x)
+        return x
