9d97955491a1114ea6b96ef437176f013e13492f
Jiachen Sun <jcsun0806@gmail.com>
2022-12-20 18:48:44 +0000

    add local loss

--- mmdet3d/models/losses/clip.py
+++ mmdet3d/models/losses/clip.py
@@ -98,33 +98,36 @@ class ClipLoss(nn.Module):
             logits_per_image = logit_scale * image_features @ text_features.T
             logits_per_text = logit_scale * text_features @ image_features.T
 
-        elif self.world_size > 1:
-            all_image_features, all_text_features = gather_features(
-                image_features, text_features,
-                self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
-
-            # if self.local_loss:
-            #     logits_per_image = logit_scale * image_features @ all_text_features.T
-            #     logits_per_text = logit_scale * text_features @ all_image_features.T
-            # else:
-            logits_per_image = logit_scale * all_image_features @ all_text_features.T
-            logits_per_text = logits_per_image.T
-
         else:
-            logits_per_image = logit_scale * image_features @ text_features.T
-            logits_per_text = logit_scale * text_features @ image_features.T
+            if self.world_size > 1:
+                all_image_features, all_text_features = gather_features(
+                    image_features, text_features,
+                    self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
+
+                # if self.local_loss:
+                #     logits_per_image = logit_scale * image_features @ all_text_features.T
+                #     logits_per_text = logit_scale * text_features @ all_image_features.T
+                # else:
+                logits_per_image = logit_scale * all_image_features @ all_text_features.T
+                logits_per_text = logits_per_image.T
+
+            else:
+                logits_per_image = logit_scale * image_features @ text_features.T
+                logits_per_text = logit_scale * text_features @ image_features.T
 
         # calculated ground-truth and cache if enabled
         num_logits = logits_per_image.shape[0]
-        if self.prev_num_logits != num_logits or device not in self.labels:
-            labels = torch.arange(num_logits, device=device, dtype=torch.long)
-            if self.world_size > 1 and self.local_loss:
-                labels = labels + num_logits * self.rank
-            if self.cache_labels:
-                self.labels[device] = labels
-                self.prev_num_logits = num_logits
-        else:
-            labels = self.labels[device]
+        # if self.prev_num_logits != num_logits or device not in self.labels:
+        #     labels = torch.arange(num_logits, device=device, dtype=torch.long)                
+        #     if self.world_size > 1 and not self.local_loss:
+        #         labels = labels + num_logits * self.rank
+        #     if self.cache_labels:
+        #         self.labels[device] = labels
+        #         self.prev_num_logits = num_logits
+        # else:
+        #     labels = self.labels[device]
+
+        labels = torch.arange(num_logits, device=device, dtype=torch.long)
 
         total_loss = (
             F.cross_entropy(logits_per_image, labels) +
