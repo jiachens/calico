29a4b4a40b57a34b001a4d91d90183de44fa75e1
Jiachen Sun <jcsun0806@gmail.com>
2023-01-05 05:29:51 +0000

    add new feature

--- mmdet3d/models/fusion_models/bevfusion.py
+++ mmdet3d/models/fusion_models/bevfusion.py
@@ -293,6 +293,7 @@ class BEVFusion(Base3DFusionModel):
                     metas,
                 )
                 if points_2 is not None:
+                    print("i am here")
                     feature_camera_2 = self.extract_camera_features(
                         img,
                         points_2,
